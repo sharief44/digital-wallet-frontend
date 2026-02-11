c1b81360767597698804174324a7990ce8ff79c4
sharief44 <shariefsk.04@gmail.com>
2026-02-11 20:57:00 +0530

    updated backend url for production

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8081", // your Spring Boot URL
+ baseURL: "https://digital-wallet-backend-lbdp.onrender.com"
+ // your Spring Boot URL
 });
 
 // Attach JWT automatically
