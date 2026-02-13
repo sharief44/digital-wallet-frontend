dab8b3a86ba0c4e66f38255836310e1721f403e0
sharief44 <shariefsk.04@gmail.com>
2026-02-13 23:59:46 +0530

    created admin panel

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import PrivateRoute from "./utils/PrivateRoute";
@@ -6,20 +6,34 @@ import Signup from "./pages/Signup";
 import AdminDashboard from "./pages/AdminDashboard";
 
 function App() {
+  const role = localStorage.getItem("role");
+
   return (
     <BrowserRouter>
       <Routes>
+        {/* Public Routes */}
         <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup/>}/>
-        <Route
-  path="/admin"
-  element={
-    localStorage.getItem("role") === "ROLE_ADMIN"
-      ? <AdminDashboard />
-      : <Navigate to="/dashboard" />
-  }
-/>
+        <Route path="/signup" element={<Signup />} />
 
+        {/* User Dashboard */}
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+
+        {/* Admin Route */}
+        <Route
+          path="/admin"
+          element={
+            role === "ROLE_ADMIN"
+              ? <AdminDashboard />
+              : <Navigate to="/dashboard" />
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
